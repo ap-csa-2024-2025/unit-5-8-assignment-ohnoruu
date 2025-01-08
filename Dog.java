83d7ab28dfd426974e95a7a777e252e36c46b43f
RubyD <143292762+ohnoruu@users.noreply.github.com>
2025-01-08 19:04:21 +0000

    yippee gutrhug hturocunh

--- Dog.java
+++ Dog.java
@@ -5,12 +5,30 @@ public class Dog
   private static int numDogs = 0;
 
   // Non-default Dog constructor
-
+  public Dog(String name){
+    this.name = name;
+    numDog++;
+    this.ID = numDogs*numDogs;
+  }
   // Default Dog constructor
 
   // Setters - instance variables only
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public void setID(int ID){
+    this.ID = ID;
+  }
 
   // Getters - instance variables only
+  public String getName(){
+    return name;
+  }
+
+  public int getID(){
+    return ID;
+  }
 
   // method getNumDogs
 }
